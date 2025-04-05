bcde6dce083fe8715245c1b23e56c997205824cc
oksupost <36145919+oksupost@users.noreply.github.com>
2025-04-05 15:51:23 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,9 @@ from openai import OpenAI
 # Show title and description.
 st.title("💬 과천 맛집 여행 챗봇")
 
+# 모델에 대한 내용을 작성한다-yos
 # 챗봇의 설명을 추가한다.
+
 st.write(
     "🐨🦘 아름다운 도시 과천의 맛집을 소개하는 챗봇입니다! 🦘🐨"
     "이 앱을 사용하기 위해서는 OpenAI API 키가 필요하며, "
@@ -12,33 +14,41 @@ st.write(
     "이 앱을 단계별로 만드는 방법은 [우리의 튜토리얼을 따라해보세요](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
 )
 
-# OpenAI API 키를 입력받는다.
-openai_api_key = st.text_input("OpenAI API Key", type="password")
+# api_key 주의한다-yos
 
 if not openai_api_key:
-    st.info("계속하려면 OpenAI API 키를 입력해주세요.", icon="🗝️")
+    st.info("Please add your OpenAI API key to continue.", icon="🗝️")
 else:
-    # OpenAI 클라이언트 생성
+
+    # Create an OpenAI client.
     client = OpenAI(api_key=openai_api_key)
 
-    # 채팅 메시지를 저장할 세션 상태 변수를 생성
+    # Create a session state variable to store the chat messages. This ensures that the
+    # messages persist across reruns.
     if "messages" not in st.session_state:
         st.session_state.messages = []
 
-    # 기존 채팅 메시지 표시
+    # Display the existing chat messages via `st.chat_message`.
     for message in st.session_state.messages:
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
 
-    # 사용자 입력을 받는 채팅 입력 필드 생성
-    if prompt := st.chat_input("무엇을 도와드릴까요?"):
-        # 현재 프롬프트 저장 및 표시
+    # Create a chat input field to allow the user to enter a message. This will display
+    # automatically at the bottom of the page.
+    if prompt := st.chat_input("What is up?"):
+
+        
+        #api_key
+        # Store and display the current prompt.
         st.session_state.messages.append({"role": "user", "content": prompt})
         with st.chat_message("user"):
             st.markdown(prompt)
 
-        # OpenAI API를 사용하여 응답 생성
+        # Generate a response using the OpenAI API.
         stream = client.chat.completions.create(
+           
+            # 여기에 수정한다 -yos
+            # model="gpt-3.5-turbo",
             model="gpt-4o-mimi",
             messages=[
                 {"role": m["role"], "content": m["content"]}
@@ -47,7 +57,8 @@ else:
             stream=True,
         )
 
-        # 응답을 채팅에 스트리밍하고 세션 상태에 저장
+        # Stream the response to the chat using `st.write_stream`, then store it in 
+        # session state.
         with st.chat_message("assistant"):
             response = st.write_stream(stream)
         st.session_state.messages.append({"role": "assistant", "content": response})
